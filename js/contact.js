8b6a85aaabed695dc3d23674f3449b0ba9c039fb
TejasBTECHWAla <tdongare1@gmail.com>
2024-04-12 20:10:46 +0530

    added

---
+++ js/contact.js
@@ -0,0 +1,25 @@
+function submitDetails() {
+    alert("Your Message was successfully submitted!")
+}
+
+function timer(){
+    const date= new Date();
+    const time= date.toLocaleTimeString();
+    document.getElementById('clock').innerText= time;
+}
+setInterval(timer, 1000);
+timer();
+
+function timerProd1(){
+    const date= new Date();
+    const time= date.toLocaleTimeString();
+    document.getElementById('clock1').innerText= time;
+}
+setInterval(timerProd1, 1000);
+timerProd1();
+
+function timerProd2(){
+    const date= new Date();
+    const time= date.toLocaleTimeString();
+    document.getElementById('clock2').innerText= time;
+}
