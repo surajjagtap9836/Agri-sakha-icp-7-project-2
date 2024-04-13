5f92d0863a4a02ac9ccddb73f539b61d011a7c08
akshadaavhad <akshadaavhad25@gmail.com>
2024-04-13 14:09:11 +0530

    chainges in the page

--- js/about.js
+++ js/about.js
@@ -6,25 +6,24 @@ function changeimg(imgname)
 
 
 
-  /*dark and light mode
-  function darkMode(){
-                const imgDark = document.getElementById(`img-dark`);
-                imgDark.classList.add(`active`);
-
+//dark and light mode
+function darkMode(){
+  const imgDark = document.getElementById(`img-dark`);
+  imgDark.classList.add(`active`);
+  
   const imgLight= document.getElementById(`img-light`);
   imgLight.classList.remove(`active`);
-
+  
   const bodyElement=document.getElementById("body");
   bodyElement.className=`theme-dark`;
-}
-function lightMode(){
+  }
+  function lightMode(){
   const imgDark= document.getElementById(`img-dark`);
   imgDark.classList.remove(`active`);
-
+  
   const imgLight=  document.getElementById(`img-light`);
   imgLight.classList.add(`active`);
-
+  
   const bodyElement=document.getElementById(`body`);
   bodyElement.className=`theme-light`;
-
-} */  
+  }
