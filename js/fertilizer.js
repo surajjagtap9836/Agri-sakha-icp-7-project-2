14c4519f719e4f705efe65260db0721783eb75a5
achalchouke <achalchouke4@gmail.com>
2024-04-13 11:36:22 +0530

    js add in 2 card

--- js/fertilizer.js
+++ js/fertilizer.js
@@ -21,4 +21,45 @@ function lightMode(){
   bodyElement.className=`theme-light`;
 
 }
+  // increament - decriment
+
+  // card-1
+  const quantityText = document.getElementById("quantity-text");
+    
+  const showQuantity = document.getElementById("show-quantity");
+
+  const showTotal = document.getElementById("show-total");
+
+  let quantity = parseInt(quantityText.innerText);
+
+      function inc(){
+          
+          if(quantity<=5)
+          {
+              quantityText.innerText = ++quantity
+          }
+          else
+          {
+              alert("You have met offer for 25%")
+          }
   
+          showQuantity.innerText = `${quantity}*489`;
+          showTotal.innerText = `Total : ${quantity*489}`;
+      }
+      function dec(){
+          
+  
+          if(quantity>1)
+          {
+              quantityText.innerText = --quantity
+          }
+          else
+          {
+              alert("You have met not offer for 1 product")
+      }
+  
+          showQuantity.innerText = `${quantity}*489`;
+          showTotal.innerText = `Total : ${quantity*489}`;
+  }
+
+  // card-2
